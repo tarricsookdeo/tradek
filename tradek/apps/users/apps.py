9ed767bd218b00d68983ac5dad3a569cf1bbacc3
Tarric Sookdeo <tarricsookdeo@outlook.com>
2021-06-06 00:13:41 -0400

    Intial user model

--- tradek/apps/users/apps.py
+++ tradek/apps/users/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tradek.apps.users'
+    label = 'users'
+    verbose_name = 'Users'
+
+
+default_app_config = 'tradel.apps.users.UsersConfig'
